7a91bb8709ec47509e4fcbee66f86e30546eb5a6
Vincent Schüßler <v.schuessler@gmail.com>
2015-10-25 21:15:21 +0100

    Implement getStateInformation() and setStateInformation() in BlankenhainAudioProcessor

--- vst/Source/BlankenhainAudioProcessor.cpp
+++ vst/Source/BlankenhainAudioProcessor.cpp
@@ -92,10 +92,16 @@ void BlankenhainAudioProcessor::getStateInformation(MemoryBlock& destData)
 	// You should use this method to store your parameters in the memory block.
 	// You could do that either as raw data, or use the XML or ValueTree classes
 	// as intermediaries to make it easy to save and load complex data.
+	var state = getState();
+	MemoryOutputStream outputStream(destData, false);
+	JSON::writeToStream(outputStream, state, false);
 }
 
 void BlankenhainAudioProcessor::setStateInformation(const void* data, int sizeInBytes)
 {
 	// You should use this method to restore your parameters from this memory block,
 	// whose contents will have been created by the getStateInformation() call.
+	MemoryInputStream inputStream(data, sizeInBytes, false);
+	var state = JSON::parse(inputStream);
+	setState(state);
 }
