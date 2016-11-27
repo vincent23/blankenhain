529df4c5b1e7689d40a30072cb5593121c9b4af6
mrnicegyu11 <Dustin.Kaiser@gmx.net>
2016-11-27 19:22:57 +0100

    Add AHDSR processing function to example plugin

--- libblankenhain/include/InstrumentBase.h
+++ libblankenhain/include/InstrumentBase.h
@@ -2,6 +2,7 @@
 
 #include "EffectBase.h"
 #include "Constants.h"
+#include "AuxFunc.h"
 
 class VoiceState;
 
@@ -21,3 +22,62 @@ private:
 	VoiceState* voices;
 	unsigned int timeInSamples = 0;
 };
+
+
+template <typename T>
+void performAHDSR(
+	T* buffer, VoiceState& voice, unsigned int timeInSamples, unsigned int sampleIndex,
+	float attackInMs, float releaseInMs, float holdInMs = 1.f, float decayInMs = 1.f, float sustainInMs = 1.f,
+	bool useSustainEvenIfNoteStillOn = false, float normalizedSustainLevel = 1.f, float normalizedHoldLevel = 1.f
+)
+{
+	float attackInSamples = aux::millisecToSamples(attackInMs);
+	float holdInSamples = aux::millisecToSamples(holdInMs);
+	float decayInSamples = aux::millisecToSamples(decayInMs);
+	float sustainInSamples = aux::millisecToSamples(sustainInMs);
+	float releaseInSamples = aux::millisecToSamples(releaseInMs);
+	unsigned int deltaTinSamples = (timeInSamples + sampleIndex) - voice.onTime;
+
+	if (voice.isOn)
+	{
+		if (deltaTinSamples <= attackInSamples)
+			buffer[sampleIndex] *= T((deltaTinSamples / attackInSamples) * normalizedHoldLevel);
+		else if (deltaTinSamples < attackInSamples + holdInSamples)
+		{
+			buffer[sampleIndex] *= T(normalizedHoldLevel);
+		}
+		else if (deltaTinSamples < attackInSamples + holdInSamples + decayInSamples)
+			buffer[sampleIndex] *= T((normalizedSustainLevel - normalizedHoldLevel) / decayInSamples * (deltaTinSamples - attackInSamples - holdInSamples) + normalizedHoldLevel);
+		else
+		{
+			buffer[sampleIndex] *= T(normalizedSustainLevel);
+			if (useSustainEvenIfNoteStillOn && deltaTinSamples > attackInSamples + holdInSamples + decayInSamples + sustainInSamples)
+				voice.off(timeInSamples + sampleIndex);
+		}
+	}
+	else
+	{
+		unsigned int deltaToff = (timeInSamples + sampleIndex) - voice.offTime;
+		if (deltaToff < static_cast<unsigned int>(releaseInSamples))
+		{
+			float lastAmplitudeRelative = 1.f;
+
+			if (deltaTinSamples <= attackInSamples)
+				lastAmplitudeRelative = deltaTinSamples / attackInSamples * normalizedHoldLevel;
+			else if (deltaTinSamples <= attackInSamples + holdInSamples)
+				lastAmplitudeRelative = normalizedHoldLevel;
+			else if (deltaTinSamples < attackInSamples + decayInSamples + holdInSamples)
+				lastAmplitudeRelative = (normalizedSustainLevel - normalizedHoldLevel) / decayInSamples * (deltaTinSamples - attackInSamples - holdInSamples) + normalizedHoldLevel;
+			else
+				lastAmplitudeRelative = normalizedSustainLevel;
+			float releaseFactor = (1 - (deltaToff / releaseInSamples)) * lastAmplitudeRelative;
+			buffer[sampleIndex] *= T(releaseFactor);
+		}
+		else
+		{
+			// switch off note immediately
+			buffer[sampleIndex] = T(0.f);
+			voice.isSilent = true;
+		}
+	}
+}
--- libblankenhain/src/ExampleInstrument.cpp
+++ libblankenhain/src/ExampleInstrument.cpp
@@ -1,5 +1,5 @@
 #include "ExampleInstrument.h"
-
+#include "InterpolatedValue.h"
 #include "ParameterBundle.h"
 #include "FloatParameter.h"
 #include "VoiceState.h"
@@ -7,26 +7,40 @@
 #include <cmath>
 
 ExampleInstrument::ExampleInstrument()
-	: InstrumentBase(1, 4)
+	: InstrumentBase(8, 4)
 {
-	getPointerToParameterBundle()->getParameter(0) = new FloatParameter(1.f, NormalizedRange(0.01f, 12.f, 1.f), "ratio", "");
-}
+	ParameterBundle* params = getPointerToParameterBundle();
 
+	params->getParameter(0) = new FloatParameter(50.f, NormalizedRange(1.f, 1700.f, 0.3f), "attack", "ms");
+	params->getParameter(1) = new FloatParameter(100.f, NormalizedRange(1.f, 1700.f, 0.3f), "hold", "ms");
+	params->getParameter(2) = new FloatParameter(1.f, NormalizedRange(), "holdlevel", "ratio");
+	params->getParameter(3) = new FloatParameter(100.f, NormalizedRange(1.f, 1700.f, 0.3f), "decay", "ms");
+	params->getParameter(4) = new FloatParameter(0.f, NormalizedRange(), "sustainBool", "bool");
+	params->getParameter(5) = new FloatParameter(100.f, NormalizedRange(1.f, 1700.f, 0.3f), "sustain", "ms");
+	params->getParameter(6) = new FloatParameter(1.0f, NormalizedRange(), "sustainLevel", "ratio");
+	params->getParameter(7) = new FloatParameter(100.f, NormalizedRange(1.f, 1700.f, 0.3f), "release", "ms");
+
+}
 
 void ExampleInstrument::processVoice(VoiceState& voice, unsigned int timeInSamples, Sample* buffer, unsigned int numberOfSamples)
 {
+
+	float attack = getParameterValue(0).get();
+	float hold = getParameterValue(1).get();
+	float holdLevel = getParameterValue(2).get();
+	float decay = getParameterValue(3).get();
+	bool sustainOn = getParameterValue(4).get() > 0.5 ? true : false;
+	float sustainLevel = getParameterValue(6).get();
+	float sustain = getParameterValue(5).get();
+	float release = getParameterValue(7).get();
+	
 	// assume sample rate = 44100
 	float f_base = 440.f / 44100;
 	float f = exp2((float(voice.key) - 69)/ 12) * f_base;
 	float tau = 2.f * acos(-1.f);
 	for (unsigned int sampleIndex = 0; sampleIndex < numberOfSamples; sampleIndex++) {
-		if (voice.isOn) {
-			unsigned int deltaT = (timeInSamples + sampleIndex) - voice.onTime;
-			buffer[sampleIndex] = Sample(sin(tau * f * float(deltaT)));
-		}
-		else {
-			// switch off note immediately
-			voice.isSilent = true;
-		}
+		unsigned int deltaT = (timeInSamples + sampleIndex) - voice.onTime;
+		buffer[sampleIndex] = Sample(sin(tau * f * float(deltaT)));
+		performAHDSR<Sample>(buffer, voice, timeInSamples, sampleIndex, attack, release, hold, decay, sustain, sustainOn, sustainLevel, holdLevel);
 	}
 }
