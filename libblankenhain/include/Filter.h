aa6e43ea932676a35e10881054c5c42940d966d4
Vincent Schüßler <v.schuessler@gmail.com>
2016-12-04 13:16:04 +0100

    Add shelf and bell filters (for EQ)

--- libblankenhain/include/Filter.h
+++ libblankenhain/include/Filter.h
@@ -9,19 +9,27 @@ class Filter : AlignedType
 {
 public:
 	void recomputeCoefficients(double frequency, double Q);
+	void recomputeCoefficientsBell(double frequency, double Q, double gainInDb);
+	void recomputeCoefficientsLowShelf(double frequency, double Q, double gainInDb);
+	void recomputeCoefficientsHighShelf(double frequency, double Q, double gainInDb);
 	Sample tickLow(const Sample& in);
 	Sample tickHigh(const Sample& in);
 	Sample tickBand(const Sample& in);
 	Sample tickNotch(const Sample& in);
+	Sample tickBell(const Sample& in);
+	Sample tickLowShelf(const Sample& in);
+	Sample tickHighShelf(const Sample& in);
+
+	// TODO do we need these?
 	Sample tickPeak(const Sample& in);
 	Sample tickAll(const Sample& in);
-	
-	// TODO bell, shelf?
 
 private:
 	void tick(const Sample& v0);
+	void recomputeCoefficientsInternal(double frequency, double Q, double gFactor);
 
-	double g, k, a1, a2, a3;
+	Sample g, k, a1, a2, a3;
+	Sample m0, m1, m2;
 	Sample ic1eq, ic2eq;
 	Sample v1, v2, v3;
 };
--- libblankenhain/src/Filter.cpp
+++ libblankenhain/src/Filter.cpp
@@ -6,11 +6,31 @@
 
 void Filter::recomputeCoefficients(double frequency, double Q)
 {
-	g = std::tan(constants::pi * frequency / constants::sampleRate);
-	k = 1. / Q;
-	a1 = 1. / (1. + g * (g + k));
-	a2 = g * a1;
-	a3 = g * a2;
+	recomputeCoefficientsInternal(frequency, Q, 1.);
+}
+
+void Filter::recomputeCoefficientsBell(double frequency, double Q, double gainInDb)
+{
+	double A = std::pow(10, gainInDb / 40.);
+	recomputeCoefficientsInternal(frequency, Q * A, 1.);
+	m1 = k * (A * A - 1.);
+}
+
+void Filter::recomputeCoefficientsLowShelf(double frequency, double Q, double gainInDb)
+{
+	double A = std::pow(10, gainInDb / 40.);
+	recomputeCoefficientsInternal(frequency, Q, 1. / std::sqrt(A));
+	m1 = k * (A - 1);
+	m2 = Sample(A * A - 1);
+}
+
+void Filter::recomputeCoefficientsHighShelf(double frequency, double Q, double gainInDb)
+{
+	double A = std::pow(10, gainInDb / 40.);
+	recomputeCoefficientsInternal(frequency, Q, std::sqrt(A));
+	m0 = Sample(A * A);
+	m1 = Sample(k * (1 - A) * A);
+	m2 = Sample(1 - A * A);
 }
 
 Sample Filter::tickLow(const Sample& in)
@@ -22,7 +42,7 @@ Sample Filter::tickLow(const Sample& in)
 Sample Filter::tickHigh(const Sample& in)
 {
 	tick(in);
-	return in - v1 * k - v2;
+	return in - k * v1 - v2;
 }
 
 Sample Filter::tickBand(const Sample& in)
@@ -34,26 +54,56 @@ Sample Filter::tickBand(const Sample& in)
 Sample Filter::tickNotch(const Sample& in)
 {
 	tick(in);
-	return in - v1 * k;
+	return in - k * v1;
+}
+
+Sample Filter::tickBell(const Sample& in)
+{
+	return in + m1 * v1;
+}
+
+Sample Filter::tickLowShelf(const Sample& in)
+{
+	return in + m1 * v1 + m2 * v2;
+}
+
+Sample Filter::tickHighShelf(const Sample& in)
+{
+	return m0 * in + m1 * v1 + m2 * v2;
 }
 
 Sample Filter::tickPeak(const Sample& in)
 {
 	tick(in);
-	return in - v1 * k - v2 * 2.;
+	return in - k * v1 - v2 * 2.;
 }
 
 Sample Filter::tickAll(const Sample& in)
 {
 	tick(in);
-	return in - v1 * (2. * k);
+	return in - k * v1 * 2.;
 }
 
 void Filter::tick(const Sample& v0)
 {
 	v3 = v0 - ic2eq;
-	v1 = ic1eq * a1 + v3 * a2;
-	v2 = ic2eq + ic1eq * a2 + v3 * a3;
-	ic1eq = v1 * 2.- ic1eq;
+	v1 = a1 * ic1eq + a2 * v3;
+	v2 = ic2eq + a2 * ic1eq + a3 * v3;
+	ic1eq = v1 * 2. - ic1eq;
 	ic2eq = v2 * 2. - ic2eq;
+}
+
+void Filter::recomputeCoefficientsInternal(double frequency, double Q, double gFactor)
+{
+	double g_ = std::tan(constants::pi * frequency / constants::sampleRate) * gFactor;
+	double k_ = 1. / Q;
+	double a1_ = 1. / (1. + g_ * (g_ + k_));
+	double a2_ = g_ * a1_;
+	double a3_ = g_ * a2_;
+
+	g = Sample(g_);
+	k = Sample(k_);
+	a1 = Sample(a1_);
+	a2 = Sample(a2_);
+	a3 = Sample(a3_);
 }
