6911ed8cc8b32dd57d399f7546ddf1a64bf23020
Vincent Schüßler <v.schuessler@gmail.com>
2017-02-15 15:13:25 +0100

    Remove mono mode from pan (may break stuff) and add boost to match ableton

--- libblankenhain/src/PanEffect.cpp
+++ libblankenhain/src/PanEffect.cpp
@@ -2,41 +2,26 @@
 
 #include "ParameterBundle.h"
 #include "InterpolatedValue.h"
+#include "AuxFunc.h"
 
-PanEffect::PanEffect() : EffectBase(2u)
+PanEffect::PanEffect() : EffectBase(1u)
 {
 	ParameterBundle* params = getPointerToParameterBundle();
 	(params->getParameter(0)) = new FloatParameter(0.f, NormalizedRange(-50.f, 50.f), "pan", "");
-	(params->getParameter(1)) = new FloatParameter(0.f, NormalizedRange(), "mono", "bool");
 }
 
 void PanEffect::process(Sample* buffer, size_t numberOfSamples)
 {
 	InterpolatedValue<float>& panningValue = getInterpolatedParameter(0);
-	bool mono = getInterpolatedParameter(1).get() > 0.5 ? true : false;
-
-	if (mono)
+	for (size_t bufferIteration = 0u; bufferIteration < numberOfSamples; bufferIteration++)
 	{
-		for (size_t bufferIteration = 0u; bufferIteration < numberOfSamples; bufferIteration++)
-		{
-			alignas(16) double lr[2];
-			buffer[bufferIteration].store_aligned(lr);
-			lr[1] = lr[0] * (1.f + (0.0f < panningValue.get() ? 0.0f : panningValue.get()) * 0.02f);
-			lr[0] = lr[0] * (1.f - (0.0f < panningValue.get() ? panningValue.get() : 0.0f) * 0.02f);
-			buffer[bufferIteration] = Sample::load_aligned(lr);
-			nextSample();
-		}
-	}
-	else
-	{
-		for (size_t bufferIteration = 0u; bufferIteration < numberOfSamples; bufferIteration++)
-		{
-			alignas(16) double lr[2];
-			buffer[bufferIteration].store_aligned(lr);
-			lr[0] = lr[0] * (1.f - (0.0f < panningValue.get() ? panningValue.get() : 0.0f) * 0.02f);
-			lr[1] = lr[1] * (1.f + (0.0f < panningValue.get() ? 0.0f : panningValue.get()) * 0.02f);
-			buffer[bufferIteration] = Sample::load_aligned(lr);
-			nextSample();
-		}
+		float panValue = panningValue.get() * .02f;
+		float boost = aux::decibelToLinear(3.f);
+		float left = aux::max(0.f, -panValue);
+		float right = aux::max(0.f, panValue);
+		float panLeft = ((1.f - left) + left * boost) * (1.f - right);
+		float panRight = ((1.f - right) + right * boost) * (1.f - left);
+		buffer[bufferIteration] *= Sample(panLeft, panRight);
+		nextSample();
 	}
 }
