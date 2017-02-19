76eeae78d948e40045cc43eeb01a6e258ed1f526
Vincent Schüßler <v.schuessler@gmail.com>
2017-02-19 22:40:07 +0100

    Add stereo spread to freeverb

--- libblankenhain/include/freeverbEffect.h
+++ libblankenhain/include/freeverbEffect.h
@@ -18,36 +18,31 @@ public:
 	void process(Sample* buffer, size_t numberOfSamples) override;
 
 protected:
-
-
 	// Include optional additional stuff
-	
-  const int nCombs = 8;
-  const int nAllpasses = 4;
-  const int stereoSpread = 23;
-  // Delay line lengths for 44100Hz sampling rate.
-  unsigned int cDelayLengths[8] = { 1617, 1557, 1491, 1422, 1356, 1277, 1188, 1116 };
-  unsigned int aDelayLengths[4] = { 225, 556, 441, 341 };
 
-  // Set static delay line lengths
-  const float fixedGain = 0.015f;
-  const float scaleDamp = 0.4f;
-  const float scaleRoom = 0.28f;
-  const float offsetRoom = 0.7f;
+	const unsigned int nCombs = 8;
+	const unsigned int nAllpasses = 4;
+	const unsigned int stereoSpread = 23;
+	// Delay line lengths for 44100Hz sampling rate.
+	unsigned int cDelayLengths[8] = { 1617, 1557, 1491, 1422, 1356, 1277, 1188, 1116 };
+	unsigned int aDelayLengths[4] = { 225, 556, 441, 341 };
 
+	// Set static delay line lengths
+	const float fixedGain = 0.015f;
+	const float scaleDamp = 0.4f;
+	const float scaleRoom = 0.28f;
+	const float offsetRoom = 0.7f;
 
+	const Sample g_ = Sample(0.5f); // allpass coefficient, immutable in FreeVerb
 
-  const Sample g_ = Sample(0.5f); // allpass coefficient, immutable in FreeVerb
-  const Sample g_plus_one = Sample(1.5f);
+	// LBFC: Lowpass Feedback Comb Filters
+	CircularBuffer<Sample>* combDelay_[8];
+	CircularBuffer<Sample>* combDelayRight[8];
 
-  Sample lastFrame_;
+	OnePoleFilter<Sample>* combLP_[8];
 
-  // LBFC: Lowpass Feedback Comb Filters
-  CircularBuffer<Sample>** combDelay_;
-
-  OnePoleFilter<Sample>** combLP_;
-
-  // AP: Allpass Filters
-  CircularBuffer<Sample>** allPassDelay_;
+	// AP: Allpass Filters
+	CircularBuffer<Sample>* allPassDelay_[4];
+	CircularBuffer<Sample>* allPassDelayRight[4];
 
 };
--- libblankenhain/src/freeverbEffect.cpp
+++ libblankenhain/src/freeverbEffect.cpp
@@ -16,104 +16,99 @@ freeverbEffect::freeverbEffect() : EffectBase(NUMBER_OF_PARAMETERS)
 	(params->getParameter(2)) = new FloatParameter(0.f, NormalizedRange(), "width", "");
 	(params->getParameter(3)) = new FloatParameter(0.f, NormalizedRange(true), "mode", "bool");
 	(params->getParameter(4)) = new FloatParameter(-6.f, NormalizedRange(-120.f, 0.f, 5.f), "dry", "dB");
-  (params->getParameter(5)) = new FloatParameter(-60.f, NormalizedRange(-120.f, 0.f, 5.f), "wet", "dB");
+	(params->getParameter(5)) = new FloatParameter(-60.f, NormalizedRange(-120.f, 0.f, 5.f), "wet", "dB");
 
-  ///////////////////////
-  // More stuff //         
+	///////////////////////
+	// More stuff //
 
-  combDelay_ = new CircularBuffer<Sample>*[nCombs];
-  allPassDelay_ = new CircularBuffer<Sample>*[nAllpasses];
-  combLP_ = new OnePoleFilter<Sample>*[nCombs];
-
-
-  for (size_t i = 0u; static_cast<int>(i) < this->nCombs; i++)
-  {
-    combDelay_[i] = new CircularBuffer<Sample>(cDelayLengths[i]);
-    combLP_[i] = new OnePoleFilter<Sample>(Sample(0.5));
-  }
-  for (size_t i = 0u; i < static_cast<size_t>(this->nAllpasses); i++)
-  {
-    allPassDelay_[i] = new CircularBuffer<Sample>(aDelayLengths[i]);
-  }
+	for (size_t i = 0u; static_cast<int>(i) < this->nCombs; i++)
+	{
+		combDelay_[i] = new CircularBuffer<Sample>(cDelayLengths[i]);
+		combDelayRight[i] = new CircularBuffer<Sample>(stereoSpread);
+		combLP_[i] = new OnePoleFilter<Sample>(Sample(0.5));
+	}
+	for (size_t i = 0u; i < static_cast<size_t>(this->nAllpasses); i++)
+	{
+		allPassDelay_[i] = new CircularBuffer<Sample>(aDelayLengths[i]);
+		allPassDelayRight[i] = new CircularBuffer<Sample>(stereoSpread);
+	}
 
 }
 
 freeverbEffect::~freeverbEffect()
 {
-  for (size_t i = 0u; static_cast<int>(i) < this->nCombs; i++)
-  {
-    delete combDelay_[i];
-    delete combLP_[i];
-  }
-  for (size_t i = 0u; i < static_cast<size_t>(this->nAllpasses); i++)
-  {
-    delete allPassDelay_[i];
-  }
-  delete[] combDelay_;
-  delete[] allPassDelay_;
-  delete[] combLP_;
-
+	for (unsigned int i = 0u; i < this->nCombs; i++)
+	{
+		delete combDelay_[i];
+		delete combLP_[i];
+		delete combDelayRight[i];
+	}
+	for (unsigned int i = 0u; i < this->nAllpasses; i++)
+	{
+		delete allPassDelay_[i];
+		delete allPassDelayRight[i];
+	}
 }
 
 
 void freeverbEffect::process(Sample* buffer, size_t numberOfSamples)
 {
-  InterpolatedValue<float>& roomSize = getInterpolatedParameter(0);
+	InterpolatedValue<float>& roomSize = getInterpolatedParameter(0);
+	InterpolatedValue<float>& width = getInterpolatedParameter(2);
+
+	const bool mode = getInterpolatedParameter(3).get() > 0.5 ? true : false;
+
+	float damping = getInterpolatedParameter(1).get();
+	damping = mode ? 0.f : damping * scaleDamp;
+	const Sample currentRoomSize = Sample(mode ? 1.f : (roomSize.get() * scaleRoom) + offsetRoom);
+	InterpolatedValue<float>& dry = getInterpolatedParameter(4);
+	InterpolatedValue<float>& wet = getInterpolatedParameter(5);
 
 
-
-  InterpolatedValue<float>& width = getInterpolatedParameter(2);
-
-  const bool mode = getInterpolatedParameter(3).get() > 0.5 ? true : false;
-
-  float damping = getInterpolatedParameter(1).get();
-  damping = mode ? 0.f : damping * scaleDamp;
-  const Sample currentRoomSize = Sample(mode ? 1.f : (roomSize.get() * scaleRoom) + offsetRoom);
-  InterpolatedValue<float>& dry = getInterpolatedParameter(4);
-
-  InterpolatedValue<float>& wet = getInterpolatedParameter(5);
-
-
-  // set low pass filter for delay output
-  for (int i = 0; i < nCombs; i++) 
-  {
-    combLP_[i]->setParams(Sample(1.0 - damping), Sample(-1.0 * damping));
-  }
+	// set low pass filter for delay output
+	for (unsigned int i = 0; i < nCombs; i++)
+	{
+		combLP_[i]->setParams(Sample(1.0 - damping), Sample(-1.0 * damping));
+	}
 
 	for (size_t i = 0; i < numberOfSamples; i++)
 	{
-
-
 		Sample fInput = buffer[i];
 		Sample out(0.0);
 
 		// Parallel LBCF filters
-		for (int j = 0; j < nCombs; j++) 
+		for (unsigned int j = 0; j < nCombs; j++)
 		{
-		  // Left channel
-		  Sample yn = fInput + (currentRoomSize * combLP_[j]->tick(combDelay_[j]->get()));
-		  combDelay_[j]->pushpop(yn);
-		  out += yn;
-
+			// Left channel
+			Sample delayed = combDelay_[j]->get();
+			Sample delayedRight = combDelayRight[j]->get();
+			delayed.replaceRightChannel(delayedRight);
+			Sample yn = fInput + (currentRoomSize * combLP_[j]->tick(delayed));
+			Sample intermediate = combDelay_[j]->pushpop(yn);
+			combDelayRight[j]->push(intermediate);
+			out += yn;
 		}
 
 		// Series allpass filters
-		for (int j = 0; j < nAllpasses; j++) 
+		for (unsigned int j = 0; j < nAllpasses; j++)
 		{
-		  // Left channel
-		  Sample vn_m = allPassDelay_[j]->get();
-		  Sample vn = out + (g_ * vn_m);
-		  allPassDelay_[j]->pushpop(vn);
+			// Left channel
+			Sample vn_m = allPassDelay_[j]->get();
+			Sample vn_m_right = allPassDelayRight[j]->get();
+			vn_m.replaceRightChannel(vn_m_right);
+			Sample vn = out + g_ * vn_m;
+			Sample intermediate = allPassDelay_[j]->pushpop(vn);
+			allPassDelayRight[j]->push(intermediate);
 
-		  // calculate output
-		  out = g_plus_one *vn_m - vn;
-
+			// calculate output
+			out = vn_m - out;
 		}
 
 		// Mix output
-		out = out * Sample(aux::decibelToLinear(wet.get())) + buffer[i] * Sample(aux::decibelToLinear(dry.get()));
-
-		buffer[i] = out;
+		float wetIn = aux::decibelToLinear(wet.get());
+		Sample wet1(wetIn * (width.get() * .5f + .5f));
+		Sample wet2(wetIn * (.5f - width.get() * .5f));
+		buffer[i] = wet1 * out + wet2 * out.flippedChannels() + buffer[i] * Sample(aux::decibelToLinear(dry.get()));
 		nextSample();
 	}
 }
