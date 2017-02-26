786bdb2e1cfa58f0ce31516fe949aa40ffe043e1
Dustin Kaiser <Dustin.Kaiser@gmx.net>
2017-02-26 11:13:15 +0100

    work on NoiseGateEffect. Still doesnt work, but problem is in EffectBase

--- libblankenhain/src/NoiseGateEffect.cpp
+++ libblankenhain/src/NoiseGateEffect.cpp
@@ -6,31 +6,33 @@
 #include "BhMath.h"
 
 NoiseGateEffect::NoiseGateEffect()
-	: EffectBase(5u, false)
+	: EffectBase(6u, false)
 	, lookaheadBuffer(static_cast<unsigned int>(aux::millisecToSamples(5.1f)))
 	, RMSVolLength(static_cast<unsigned int>(aux::millisecToSamples(50.f)))
-	, lastStartTime(0u)
+	, lastStartTime(1u)
 	, lastStopTime(0u)
 {
 	ParameterBundle* params = getPointerToParameterBundle();
 	(params->getParameter(0)) = new FloatParameter(-6.f, NormalizedRange(-120.f, 0.f, 5.f), "threshold", "dB");
-  (params->getParameter(1)) = new FloatParameter(-119.f, NormalizedRange(-120.f, 0.f, 5.f), "bottom", "dB");
-	(params->getParameter(2)) = new FloatParameter(1.f, NormalizedRange(0.05f, 200.f, 0.8f), "attack", "ms");
-	(params->getParameter(3)) = new FloatParameter(1.f, NormalizedRange(0.05f, 200.f, 0.8f), "release", "ms");
-	(params->getParameter(4)) = new FloatParameter(0.f, NormalizedRange(0.0f, 5.f, 0.8f), "lookahead", "ms");
+	(params->getParameter(1)) = new FloatParameter(-119.f, NormalizedRange(-120.f, 0.f, 5.f), "bottom", "dB");
+	(params->getParameter(2)) = new FloatParameter(50.f, NormalizedRange(0.f, 100.f, 5.f), "rmsVolLength", "ms");
+	(params->getParameter(3)) = new FloatParameter(1.f, NormalizedRange(0.05f, 200.f, 0.8f), "attack", "ms");
+	(params->getParameter(4)) = new FloatParameter(1.f, NormalizedRange(0.05f, 200.f, 0.8f), "release", "ms");
+	(params->getParameter(5)) = new FloatParameter(0.f, NormalizedRange(0.0f, 5.f, 0.8f), "lookahead", "ms");
 }
 
 void NoiseGateEffect::process(Sample* buffer, size_t numberOfSamples, size_t currentTime)
 {
-	const float thresholdInDb = getInterpolatedParameter(0).get();
-  const float bottomInDb = getInterpolatedParameter(1).get();
-	const float attackInMs = getInterpolatedParameter(2).get();
-	const float releaseInMs = getInterpolatedParameter(3).get();
-	const float lookaheadnMs = getInterpolatedParameter(4).get();
-	const unsigned int lookaheadInSamples = static_cast<unsigned int>(aux::millisecToSamples(lookaheadnMs));
-	const unsigned int delayLineLength = RMSVolLength + lookaheadInSamples;
+	const float threshold = aux::decibelToLinear(getInterpolatedParameter(0).get());
+	const float bottom = aux::decibelToLinear(getInterpolatedParameter(1).get());
+	const float attack = aux::millisecToSamples(getInterpolatedParameter(3).get());
+	const float release = aux::millisecToSamples(getInterpolatedParameter(4).get());
+	const float lookahead = aux::millisecToSamples(getInterpolatedParameter(5).get());
+	const unsigned int lookaheadInSamples = static_cast<unsigned int>(lookahead);
+	const unsigned int delayLineLength = static_cast<unsigned int>(aux::millisecToSamples(getInterpolatedParameter(2).get()))
+		+ lookaheadInSamples;
 
-  this->delayEffectProducesInSamples = delayLineLength;
+	this->delayEffectProducesInSamples = delayLineLength;
 
 	lookaheadBuffer.setSize(delayLineLength);
 	
@@ -43,46 +45,48 @@ void NoiseGateEffect::process(Sample* buffer, size_t numberOfSamples, size_t cur
 
 		const Sample RMScurrent = (RMSkeeper / Sample(static_cast<float>(delayLineLength))).sqrt();
 
-    buffer[bufferIteration] = BufferOut;
+		buffer[bufferIteration] = BufferOut;
 
-		if (RMScurrent.maxValue() > aux::decibelToLinear(thresholdInDb))
+		if (RMScurrent.maxValue() > threshold)
 		{
 			if (lastStopTime < lastStartTime)
-        lastStopTime = currentTime;
+			lastStopTime = currentTime;
 
-			if (currentTime - lastStopTime < aux::millisecToSamples(releaseInMs))
+			if (currentTime - lastStopTime < release)
 			{
-          // perform release, gate just turned off
-        float curFloatVolume = 1.f - aux::decibelToLinear(bottomInDb);
-        curFloatVolume /= static_cast<float>(aux::millisecToSamples(releaseInMs));
-        curFloatVolume *= static_cast<float>(currentTime - lastStartTime);
-        Sample currentVolume(curFloatVolume + aux::decibelToLinear(bottomInDb));
-        buffer[bufferIteration] *= currentVolume;
-      }
+				// perform release, gate just turned off
+				float curFloatVolume = 1.f - bottom;
+				curFloatVolume /= static_cast<float>(release);
+				curFloatVolume *= static_cast<float>(currentTime - lastStopTime);
+				Sample currentVolume(curFloatVolume + bottom);
+				buffer[bufferIteration] *= currentVolume;
+			}
 			else
 			{
-        // Gate not active
-      }
-		}
-		else if (currentTime - lastStartTime < aux::millisecToSamples(attackInMs))
-		{
-			if (lastStartTime < lastStopTime)
-				lastStartTime = currentTime;
-			// perform attack, gate just turned on
-      float curFloatVolume = 1.f - aux::decibelToLinear(bottomInDb);
-      curFloatVolume /= static_cast<float>(aux::millisecToSamples(attackInMs));
-      curFloatVolume *= static_cast<float>(aux::millisecToSamples(attackInMs) - currentTime - lastStartTime);
-      Sample currentVolume(curFloatVolume + aux::decibelToLinear(bottomInDb));
-      buffer[bufferIteration] *= currentVolume;
+				// Gate not active
+			}
 		}
 		else
 		{
-      if (lastStartTime < lastStopTime)
-        lastStartTime = currentTime;
-			// quiet, gate active
-      buffer[bufferIteration] *= Sample(aux::decibelToLinear(bottomInDb));
+			if (lastStartTime < lastStopTime)
+				lastStartTime = currentTime;
+			
+			if (currentTime - lastStartTime < attack)
+			{
+				// perform attack, gate just turned on
+				float curFloatVolume = 1.f - bottom;
+				curFloatVolume /= static_cast<float>(attack);
+				curFloatVolume *= static_cast<float>(attack - (currentTime - lastStartTime));
+				Sample currentVolume(curFloatVolume + bottom);
+				buffer[bufferIteration] *= currentVolume;
+			}
+			else
+			{
+				// quiet, gate active
+				buffer[bufferIteration] *= Sample(bottom);
+			}
 		}
-    currentTime++;
+		currentTime++;
 		nextSample();
 	}
 }
