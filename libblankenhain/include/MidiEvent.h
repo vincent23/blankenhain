7c2ecbf04de54f55aabf1041cfc8093ffd2fbb7b
Vincent Schüßler <v.schuessler@gmail.com>
2017-02-27 19:55:19 +0100

    Fix #29: Everything is a class

--- libblankenhain/include/MidiEvent.h
+++ libblankenhain/include/MidiEvent.h
@@ -3,8 +3,9 @@
 // Changes in velocity of one note are not supported right now!
 // event with velcocity == 0u is noteOffEvent
 // event with velocity > 0u is noteOn Event
-struct MidiEvent
+class MidiEvent
 {
+public:
 	MidiEvent() = default;
 	MidiEvent(unsigned int sampleOffset, unsigned int key, unsigned int velocity);
 	bool isNoteOn() const;
