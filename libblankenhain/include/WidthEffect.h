7b830bcb1ef27d6031acbff9f29e98b90f52fcfa
mrnicegyu11 <Dustin.Kaiser@gmx.net>
2017-03-05 16:51:46 +0100

    Fix compialtion failure due to bh_width
    Is programmer's fuckup, code was fine, previous commit was not complete

--- libblankenhain/include/WidthEffect.h
+++ libblankenhain/include/WidthEffect.h
@@ -1,10 +1,18 @@
 #pragma once
 
 #include "EffectBase.h"
+#include "ParameterBundle.h"
+#include "AuxFunc.h"
+#include "InterpolatedValue.h"
+#include "Oscillators.h"
 
 class WidthEffect : public EffectBase
 {
 public:
 	WidthEffect();
-	void process(Sample* buffer, size_t numberOfSamples, size_t currentTime) override;
+	void process(Sample* buffer, size_t numberOfSamples, size_t currentTime) final;
+	void getModulation(float* modulationValues, size_t sampleOffset) final;
+
+private:
+	NaiveOscillator lfo;
 };
