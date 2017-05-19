457333d4c0a66786c896dc89cb1bbd1ba2efe32c
mrnicegyu11 <Dustin.Kaiser@gmx.net>
2017-05-19 14:51:26 +0200

    Add wavewriter. Closes #51.

--- example/Example.cpp
+++ example/Example.cpp
@@ -1,19 +1,37 @@
 #include "BhSoundtrack.h"
-
 #define VC_EXTRALEAN
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <mmsystem.h>
 #include <mmreg.h>
 
+///////////////////////////
+////// O P T I O N S //////
+///////////////////////////
+
+// Put the desired filename here
+const char* nameOfOutputFile = "blankenhainWaveWriter.wav";
+
+// Set to true to write RAW floating point audio instead of a wav file
+const bool writeLegacyRAWAudio = false;
+
+////////////////////////////
+////////////////////////////
+////////////////////////////
+
+
+
+// Setting up floating point coprocessor, probably in a naughty way
+// background: http://stackoverflow.com/questions/12517429/error-lnk2001-unresolved-external-symbol-fltused-in-wdk
 extern "C" {
 	int _fltused = 1;
 }
 
+// Taking care of VC++ compiler pure virtual function calling convention stuff.
+// background: https://msdn.microsoft.com/en-us/en-en/library/ff798096.aspx
 extern "C" int _cdecl _purecall(void) {
 	return 0;
 }
-
 
 int CALLBACK
 WinMain(HINSTANCE Instance,
@@ -21,92 +39,82 @@ WinMain(HINSTANCE Instance,
 	LPSTR CommandLine,
 	int ShowCode)
 {
+	//////////////////////////////////////
 	// Set up FPU according to entire 64k demoscene standards appareantly
-	unsigned short foo = 0u;
+	//////////////////////////////////////
+	const unsigned short foo = 0u;
 	_asm FSTCW foo
-	unsigned short fcw = 3711;
+	const unsigned short fcw = 3711;
 	__asm fldcw fcw;
+	//////////////////////////////////////
 
 	const unsigned int numberOfSamples = blankenhain::lengthInSamples();
 	float* audioBuffer = new float[numberOfSamples * 2];
-	const bool threaded = false;
-  const bool debug = false;
-  if (!debug)
-  {
-    if (threaded) {
-      CreateThread(0, 0, (LPTHREAD_START_ROUTINE)blankenhain::render, audioBuffer, 0, 0);
-      // wait one second
-      Sleep(1000);
-    }
-    else {
-      blankenhain::render(audioBuffer);
-    }
-  }
-  else
-  {
-    //Sleep(10000);
-    for (unsigned int i = 0u; i < numberOfSamples * 2; i++)
-      audioBuffer[i] = 1.f;
-  }
+	blankenhain::render(audioBuffer);
 
-  // Write Wav
-  // via http://soundfile.sapp.org/doc/WaveFormat/
-  HANDLE outputFile;
-  do {
-    outputFile = CreateFile("audio16.wav", GENERIC_WRITE, FILE_SHARE_WRITE, NULL, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
-  } while (outputFile == INVALID_HANDLE_VALUE);
+	if (!writeLegacyRAWAudio)
+	{
+		// Write Riff-Wave
+		// via http://soundfile.sapp.org/doc/WaveFormat/
+		HANDLE outputFile;
+		do {
+			outputFile = CreateFile(nameOfOutputFile, GENERIC_WRITE, FILE_SHARE_WRITE, NULL, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
+		} while (outputFile == INVALID_HANDLE_VALUE);
 
 
-  WriteFile(outputFile, "RIFF", 4, NULL, NULL);
-  const unsigned char c_subchunksize2 = blankenhain::lengthInSamples() * 2 * 2;
-  const unsigned char c_chunksize = 4 + (8 + 16) + (8 + c_subchunksize2);
-  WriteFile(outputFile, &c_chunksize, 4, NULL, NULL);
-  WriteFile(outputFile, "WAVE", 4, NULL, NULL);
-  WriteFile(outputFile, "fmt ", 4, NULL, NULL);
+		WriteFile(outputFile, "RIFF", 4, NULL, NULL);
+		const unsigned char c_subchunksize2 = blankenhain::lengthInSamples() * 2 * 2;
+		const unsigned char c_chunksize = 4 + (8 + 16) + (8 + c_subchunksize2);
+		WriteFile(outputFile, &c_chunksize, 4, NULL, NULL);
+		WriteFile(outputFile, "WAVE", 4, NULL, NULL);
+		WriteFile(outputFile, "fmt ", 4, NULL, NULL);
 
-  const unsigned int c_subchunk1size = 16;
-  WriteFile(outputFile, &c_subchunk1size, 4, NULL, NULL);
-  const short s_audioformat = 1;
-  WriteFile(outputFile, &s_audioformat, 2, NULL, NULL);
-  const short s_numchannels = 2;
-  WriteFile(outputFile, &s_numchannels, 2, NULL, NULL);
-  const unsigned int c_samplerate = 44100;
-  WriteFile(outputFile, &c_samplerate, 4, NULL, NULL);
-  const unsigned int c_byterate = 44100 * 2 * 16 / 8;
-  WriteFile(outputFile, &c_byterate, 4, NULL, NULL);
+		const unsigned int c_subchunk1size = 16;
+		WriteFile(outputFile, &c_subchunk1size, 4, NULL, NULL);
+		const short s_audioformat = 1;
+		WriteFile(outputFile, &s_audioformat, 2, NULL, NULL);
+		const short s_numchannels = 2;
+		WriteFile(outputFile, &s_numchannels, 2, NULL, NULL);
+		const unsigned int c_samplerate = 44100;
+		WriteFile(outputFile, &c_samplerate, 4, NULL, NULL);
+		const unsigned int c_byterate = 44100 * 2 * 16 / 8;
+		WriteFile(outputFile, &c_byterate, 4, NULL, NULL);
 
-  const short s_blockalign = 4;
-  WriteFile(outputFile, &s_blockalign, 2 , NULL, NULL);
-  const short s_bitspersample = 16;
-  WriteFile(outputFile, &s_bitspersample, 2 , NULL, NULL);
-  WriteFile(outputFile, "data", 4, NULL, NULL);
-  WriteFile(outputFile, &c_subchunksize2, 4, NULL, NULL);
-  
-  __int16* data = new __int16[blankenhain::lengthInSamples() * 2];
-  for (unsigned int i = 0u; i < blankenhain::lengthInSamples() * 2; i = i + 2)
-  {
-    data[i / 2u]                                  = static_cast<__int16>( (audioBuffer[i]     + 1.f) / 0.5f * 65535.f) - 32768;
-    data[i / 2u + blankenhain::lengthInSamples()] = static_cast<__int16>( (audioBuffer[i + 1] + 1.f) / 0.5f * 65535.f) - 32768;
-  }
-  WriteFile(outputFile, data, sizeof(__int16) * blankenhain::lengthInSamples() * 2, NULL, NULL);
-  CloseHandle(outputFile);
-  if (data != nullptr)
-  {
-    delete[] data;
-    data = nullptr;
-  }
+		const short s_blockalign = 4;
+		WriteFile(outputFile, &s_blockalign, 2, NULL, NULL);
+		const short s_bitspersample = 16;
+		WriteFile(outputFile, &s_bitspersample, 2, NULL, NULL);
+		WriteFile(outputFile, "data", 4, NULL, NULL);
+		WriteFile(outputFile, &c_subchunksize2, 4, NULL, NULL);
 
-  //old code, write raw audio
-	//HANDLE outputFile;
-	//do {
-	//	outputFile = CreateFile("audio.out", GENERIC_WRITE, FILE_SHARE_WRITE, NULL, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
-	//} while (outputFile == INVALID_HANDLE_VALUE);
-	//WriteFile(outputFile, audioBuffer, sizeof(float) * numberOfSamples * 2, NULL, NULL);
-	//CloseHandle(outputFile);
-
+		__int16* audioBufferAs16BitInt = new __int16[blankenhain::lengthInSamples() * 2];
+		for (unsigned int i = 0u; i < blankenhain::lengthInSamples() * 2; i++)
+		{
+			float& fval = (audioBuffer[i]);
+			__int16 value = static_cast<__int16>((fval + 1.f) / 2.f * 65535.f) - 32768;
+			audioBufferAs16BitInt[i] = value;
+		}
+		WriteFile(outputFile, audioBufferAs16BitInt, sizeof(__int16) * blankenhain::lengthInSamples() * 2, NULL, NULL);
+		CloseHandle(outputFile);
+		if (audioBufferAs16BitInt != nullptr)
+		{
+			delete[] audioBufferAs16BitInt;
+			audioBufferAs16BitInt = nullptr;
+		}
+	}
+	else
+	{
+		// Legacy Feature
+		// Write audio as raw floating point values
+		HANDLE outputFile;
+		do {
+			outputFile = CreateFile(nameOfOutputFile, GENERIC_WRITE, FILE_SHARE_WRITE, NULL, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
+		} while (outputFile == INVALID_HANDLE_VALUE);
+		WriteFile(outputFile, audioBuffer, sizeof(float) * numberOfSamples * 2, NULL, NULL);
+		CloseHandle(outputFile);
+	}
 
 	delete audioBuffer;
-
 	return 0;
 }
 
@@ -190,7 +198,6 @@ void memset2(void* dst, unsigned char val, unsigned int count);
 static void initTerm(ePVFV *pfbegin, ePVFV *pfend);
 static void initAtExit();
 static void doAtExit();
-//int __cdecl atexit(ePVFV func);
 
 
 #pragma data_seg(".CRT$XCA")
@@ -229,20 +236,6 @@ static void initAtExit() {
 static void doAtExit() {
 	initTerm(g_atExitList, g_atExitList + MAX_ATEXITS);
 }
-
-/*
-int __cdecl atexit(ePVFV func) {
-	// get next free entry in atexist list
-	unsigned int index = 0;
-	while (g_atExitList[index++]);
-
-	if (index < MAX_ATEXITS) {
-		g_atExitList[index] = func;
-		return 0;
-	}
-
-	return -1;
-}*/
 
 void GlobalsStaticsInit() {
 #	ifdef NDEBUG
