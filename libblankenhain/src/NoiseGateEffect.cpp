8c23ed9358641dda9e89e22ddfab678a4c15963d
Dustin Kaiser <Dustin.Kaiser@gmx.net>
2018-05-21 18:01:38 +0200

    Add hold parameter to noise gate
    This enables the cool Phil Collins style gated snare drum effect, sort of.

--- libblankenhain/src/NoiseGateEffect.cpp
+++ libblankenhain/src/NoiseGateEffect.cpp
@@ -6,19 +6,20 @@
 #include "BhMath.h"
 
 NoiseGateEffect::NoiseGateEffect()
-	: EffectBase(5u, false)
+	: EffectBase(6u, false)
 	//, lookaheadBuffer(static_cast<unsigned int>(aux::millisecToSamples(60.2f)))
 	, lookaheadBuffer(1 << 12)
-	, RMSVolLength(static_cast<unsigned int>(aux::millisecToSamples(10.f)))
+	, RMSVolLength(static_cast<unsigned int>(aux::millisecToSamples(1.f)))
 	, lastStartTime(1u)
 	, lastStopTime(0u)
 {
 	ParameterBundle& params = getParameterBundle();
 	params.initParameter(0, new FloatParameter(-6.f, NormalizedRange(-120.f, 0.f, 5.f), "threshold", "dB"));
 	params.initParameter(1, new FloatParameter(-119.f, NormalizedRange(-120.f, 0.f, 5.f), "bottom", "dB"));
-	params.initParameter(2, new FloatParameter(1.f, NormalizedRange(0.05f, 200.f, 0.8f), "attack", "ms"));
-	params.initParameter(3, new FloatParameter(1.f, NormalizedRange(0.05f, 200.f, 0.8f), "release", "ms"));
-	params.initParameter(4, new FloatParameter(0.f, NormalizedRange(0.0f, 50.f, 0.8f), "lookahead", "ms"));
+	params.initParameter(2, new FloatParameter(1.f, NormalizedRange(0.05f, 300.f, 0.8f), "attack", "ms"));
+	params.initParameter(3, new FloatParameter(1.f, NormalizedRange(0.05f, 300.f, 0.8f), "release", "ms"));
+	params.initParameter(4, new FloatParameter(20.f, NormalizedRange(0.0f, 99.f, 0.8f), "lookahead", "ms"));
+	params.initParameter(5, new FloatParameter(10.f, NormalizedRange(0.0f, 1000.f, 0.8f), "hold", "ms"));
 }
 
 void NoiseGateEffect::process(Sample* buffer, size_t numberOfSamples, size_t currentTime)
@@ -28,6 +29,8 @@ void NoiseGateEffect::process(Sample* buffer, size_t numberOfSamples, size_t cur
 	const float attack = aux::millisecToSamples(interpolatedParameters.get(2));
 	const float release = aux::millisecToSamples(interpolatedParameters.get(3));
 	const float lookahead = aux::millisecToSamples(interpolatedParameters.get(4));
+	const float hold = aux::millisecToSamples(interpolatedParameters.get(5));
+
 	const unsigned int lookaheadInSamples = static_cast<unsigned int>(lookahead);
 	const unsigned int delayLineLength = RMSVolLength + lookaheadInSamples;
 
@@ -48,17 +51,19 @@ void NoiseGateEffect::process(Sample* buffer, size_t numberOfSamples, size_t cur
 
 		buffer[bufferIteration] = BufferOut;
 
+
+
 		if (RMScurrent.maxValue() > threshold)
 		{
-			if (lastStopTime < lastStartTime)
-			lastStopTime = currentTime;
+			if (lastStartTime <= lastStopTime)
+				lastStartTime = currentTime;
 
-			if (currentTime - lastStopTime < release)
+			if (currentTime - lastStartTime < attack)
 			{
-				// perform release, gate just turned off
+				// perform attack, gate just turned on
 				float curFloatVolume = 1.f - bottom;
-				curFloatVolume /= static_cast<float>(release);
-				curFloatVolume *= static_cast<float>(currentTime - lastStopTime);
+				curFloatVolume /= static_cast<float>(attack);
+				curFloatVolume *= static_cast<float>(attack - (currentTime - lastStartTime));
 				Sample currentVolume(curFloatVolume + bottom);
 				buffer[bufferIteration] *= currentVolume;
 			}
@@ -69,16 +74,21 @@ void NoiseGateEffect::process(Sample* buffer, size_t numberOfSamples, size_t cur
 		}
 		else
 		{
-			if (lastStartTime < lastStopTime)
-				lastStartTime = currentTime;
-			
-			if (currentTime - lastStartTime < attack)
+			if (lastStopTime < lastStartTime)
+				lastStopTime = currentTime;
+
+			if (currentTime - lastStopTime < release + hold)
 			{
-				// perform attack, gate just turned on
-				float curFloatVolume = 1.f - bottom;
-				curFloatVolume /= static_cast<float>(attack);
-				curFloatVolume *= static_cast<float>(attack - (currentTime - lastStartTime));
-				Sample currentVolume(curFloatVolume + bottom);
+				// perform hold and release
+				float curBottom = bottom;
+				if (currentTime - lastStopTime < hold)
+					curBottom = 1.f;
+
+				// perform release, gate just turned off
+				float curFloatVolume = 1.f - curBottom;
+				curFloatVolume /= static_cast<float>(release);
+				curFloatVolume *= static_cast<float>(release - (currentTime - lastStopTime - hold));
+				Sample currentVolume(curBottom + curFloatVolume);
 				buffer[bufferIteration] *= currentVolume;
 			}
 			else
